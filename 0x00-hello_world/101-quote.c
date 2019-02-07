fc7a8983d78dd3885186999d31b3fe7c9a7e670d
Fesus Rocuts <fesusrocuts@gmail.com>
2019-02-07 02:50:43 +0000

    prev t101 c0

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - print message whit write function
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	char[] buf;
+	char * buf = "and that piece of art is useful\"" - Dora Korpar, 2015-10-19";
+	write(1, , strlen(buf));
+	return (1);
+}
