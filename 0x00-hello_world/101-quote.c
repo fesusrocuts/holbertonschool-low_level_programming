c115993fd4eae93746487f8e501a1c3d7bffb6ff
Fesus Rocuts <fesusrocuts@gmail.com>
2019-02-07 02:57:31 +0000

    t9 c0

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,6 @@
  */
 int main(void)
 {
-	char[] buf;
-	char * buf = "and that piece of art is useful\"" - Dora Korpar, 2015-10-19";
-	write(1, , strlen(buf));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
