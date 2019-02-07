ec436f763977e8d64b23edba980036235e044d3c
Fesus Rocuts <fesusrocuts@gmail.com>
2019-02-07 00:30:37 +0000

    t4 c2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*This program is to test*/
 int main(){
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return 0;
 }
