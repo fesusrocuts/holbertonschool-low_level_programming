d8dbb82fcf4030f8bc0bbd6dd61424a57a8c5811
fesus rocuts <fesusrocuts@gmail.com>
2019-02-12 07:40:51 -0500

    1-alphabet.c script file that allow list the alphabet use prototype print_alphabet and function _putchar include in holberton.h, use for loop, i can used while loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - writes the alphabet with _putchar
+ */
+void print_alphabet(void)
+{
+	int _putchar(char c);
+	int i;
+	for(i=97;i<=122;i++){
+		_putchar(i);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/1-alphabet_while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - writes the alphabet with _putchar
+ */
+void print_alphabet(void)
+{
+	int _putchar(char c); 
+	int i=97;
+	while(i<=122){
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
