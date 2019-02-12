08400757fd592d9623eb109b1a6bea63a001baa7
fesus rocuts <fesusrocuts@gmail.com>
2019-02-12 08:19:39 -0500

    1-alphabet.c script file, add betty test and I pass

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
-
 /**
  * print_alphabet - writes the alphabet with _putchar
+ * Return: void
  */
 void print_alphabet(void)
 {
 	int i;
-	for(i='a'; i<='z'; i++)
+
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
