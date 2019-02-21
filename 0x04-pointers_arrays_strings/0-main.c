d7b82e81bd36d36551798a4636fd2b132be8e7b0
fesus rocuts <fesusrocuts@gmail.com>
2019-02-20 20:39:39 -0500

    t0 c1

--- 0x04-pointers_arrays_strings/0-main.c
+++ 0x04-pointers_arrays_strings/0-main.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
+#include <limits.h>
 
 /**
  * main - check the code for Holberton School students.
@@ -11,6 +12,26 @@ int main(void)
     int n;
 
     n = 402;
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+
+    n = -4096;
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+
+    n = INT_MAX;
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+
+    n = INT_MIN;
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+
+    n = INT_MAX / INT_MIN;
     printf("n=%d\n", n);
     reset_to_98(&n);
     printf("n=%d\n", n);
--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,9 @@
 #include "holberton.h"
-
+/**
+ * reset_to_98 - fn convert any integer to 98 value
+ * @n: get integer value
+ * return: void
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
