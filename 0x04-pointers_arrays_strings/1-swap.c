462392fb7c414e62b80461869c7d4e1d05fd1f8a
fesus rocuts <fesusrocuts@gmail.com>
2019-02-20 20:44:09 -0500

    t1 c0

---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - fn convert any integer to 98 value
+ * @n: get integer value
+ * return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int z = *a;
+	*a = *b;
+	*b = z;
+}
