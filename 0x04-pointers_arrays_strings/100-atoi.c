24408ec849dbaf8c77bd651eb4f70bdf9a0fc251
fesus rocuts <fesusrocuts@gmail.com>
2019-02-26 03:51:08 -0500

    t10 c5

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -6,22 +6,22 @@
  */
 int _atoi(char *s)
 {
-	int i = 0;
+	int iterator = 0;
 	int result = 0;
 	int c;
 	int countInt = 0;
 	int countNegative = 0;
 	int isNegative = 0;
 
-	while (s[i] != '\0')
+	while (s[iterator])
 	{
-		c = s[i] - 48;
+		c = s[iterator] - '0';
 		if (c == -3 && result == 0)
 		{
 			countNegative++;
 			if (countNegative > 1)
 				isNegative = 1;
-			else if (countNegative == 1 && i == 0)
+			else if (countNegative == 1 && iterator == 0)
 				isNegative = 1;
 		}
 		else if (c >= 0 && c <= 9)
@@ -34,14 +34,14 @@ int _atoi(char *s)
 		}
 		else if (countInt > 0)
 		{
-			i = -2;
+			iterator = -2;
 		}
 		else
 		{
 			isNegative = 0;
 			countNegative = 0;
 		}
-		i++;
+		iterator++;
 	}
 	return (result);
 }
