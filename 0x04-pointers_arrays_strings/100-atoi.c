b3265df2ab4b3740fa9777d45b0c1c91e844ec9a
fesus rocuts <fesusrocuts@gmail.com>
2019-02-26 03:12:04 -0500

    t10 c3

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -7,32 +7,41 @@
 int _atoi(char *s)
 {
 	int i = 0;
-	int r = -1;
+	int result = 0;
 	int c;
-	int nc = 0;
-	int n = 1;
+	int countInt = 0;
+	int countNegative = 0;
+	int isNegative = 0;
 
 	while (*(s + i) != '\0')
 	{
 		c = (int)*(s + i) - 48;
-		if (c == -3 && r == -1 )
+		if (c == -3 && result == 0)
 		{
-			nc++;
-			if (nc > 1)
-				n = -1;
-			else if (nc == 1 && i == 0)
-				n = -1;
+			countNegative++;
+			if (countNegative > 1)
+				isNegative = 1;
+			else if (countNegative == 1 && i == 0)
+				isNegative = 1;
 		}
 		else if (c >= 0 && c <= 9)
-			r =  r > 0 ? r * 10 + c : r + c + 1;
-		else if (r >= 0)
+		{
+			countInt++;
+			if (isNegative == 1)
+				result =  result < 0 ? result * 10 - c : result - c;
+			else
+				result =  result > 0 ? result * 10 + c : result + c;
+		}
+		else if (countInt > 0)
+		{
 			i = -2;
+		}
 		else
 		{
-			n = 1;
-			nc = 0;
+			isNegative = 0;
+			countNegative = 0;
 		}
 		i++;
 	}
-	return (i != 0 ? r * n : 0);
+	return (result);
 }
--- 0x04-pointers_arrays_strings/100-main.c
+++ 0x04-pointers_arrays_strings/100-main.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
+#include <limits.h>
 
 /**
  * main - check the code for Holberton School students.
@@ -22,11 +23,16 @@ int main(void)
 	printf("%d\n", n);
 	n = _atoi("Suite 402");
 	printf("%d\n", n);
-	n = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
 	printf("%d\n", n);
 	n = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
 	printf("%d\n", n);
+	n = _atoi("-2147483648");
+	printf("%d\n", n);
+	n = _atoi("2147483647");
+	printf("%d\n", n);
 	n = _atoi("");
+	printf("%d\n", n);
+	n = _atoi("-");
 	printf("%d\n", n);
 	return (0);
 }
