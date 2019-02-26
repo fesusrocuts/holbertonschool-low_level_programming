068147c8b7417c1a531ec218e45c324c4a1da4f6
fesus rocuts <fesusrocuts@gmail.com>
2019-02-25 22:21:45 -0500

    t8 c0

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: get array of integers
+ * @n: get integer
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf(i < n - 1 ? "%d, " : "%d\n", a[i]);
+		i++;
+	}
+}
