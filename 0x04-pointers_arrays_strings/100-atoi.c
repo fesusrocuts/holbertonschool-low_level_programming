456dfcb7f6cbca414b2b056cb9833bce2c41a270
fesus rocuts <fesusrocuts@gmail.com>
2019-02-26 01:16:55 -0500

    t10 c0

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ *  _atoi- return integer of the string
+ * @s: get string
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int r = -1;
+	int c;
+	int nc = 0;
+	int n = 1;
+
+	while (*(s + i))
+	{
+
+		c = (int)*(s + i) - 48;
+		if (c >= 0 && c <= 9)
+			r =  r > 0 ? r * 10 + c : r + c + 1;
+		else if (r >= 0)
+			i = -2;
+		else if (c == -3)
+		{
+			nc++;
+			if (nc > 1)
+				n = -1;
+			else if (nc == 1 && i == 0)
+				n = -1;
+		}
+		else
+		{
+			n = 1;
+			nc = 0;
+		}
+		i++;
+	}
+	return (r * n);
+}
