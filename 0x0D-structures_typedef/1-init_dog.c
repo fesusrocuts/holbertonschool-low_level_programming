54280836a482d67b24ce923979287ce80f2c77a4
Fesus Rocuts <fesusrocuts@gmail.com>
2019-03-12 18:01:57 +0000

    t1 c0

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - load data in struct dog
+ * @d: struct dog, base to create dog data
+ * @name: attribute string
+ * @age: attribute float
+ * @owner: attribute string
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -15,3 +15,16 @@ struct dog
 	char *owner;
 };
 #endif
+
+#ifndef _INIT_DOG_H
+#define _INIT_DOG_H
+/**
+ * init_dog - load data in struct dog
+ * @d: struct dog, base to create dog data
+ * @name: attribute string
+ * @age: attribute float
+ * @owner: attribute string
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
