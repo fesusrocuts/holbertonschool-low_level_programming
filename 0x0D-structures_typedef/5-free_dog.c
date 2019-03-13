22cba73def4ff7affbbf1f6ef592b40ac9b3173c
Fesus Rocuts <fesusrocuts@gmail.com>
2019-03-13 18:16:52 +0000

    t5 c2

--- 0x0D-structures_typedef/5-free_dog.c
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -8,8 +8,12 @@ void free_dog(dog_t *d)
 	{
 		if (d->name != NULL)
 			free(d->name);
+		if (d->age != NULL)
+			free(d->age);
 		if (d->owner != NULL)
 			free(d->owner);
 		free(d);
 	}
+	else
+		return;
 }
