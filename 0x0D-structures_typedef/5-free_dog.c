1b1bfefbc46c549b33417f89d9e71c2e3db7b981
Fesus Rocuts <fesusrocuts@gmail.com>
2019-03-13 18:10:35 +0000

    t5 c0

---
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+void free_dog(dog_t *d)
+{
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
+	free(d);
+}
