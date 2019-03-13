f38ccd283297eabc82c127c074cf27cb498cc4b6
Fesus Rocuts <fesusrocuts@gmail.com>
2019-03-13 18:20:11 +0000

    t5 c4

--- 0x0D-structures_typedef/5-free_dog.c
+++ 0x0D-structures_typedef/5-free_dog.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+* free_dog - free memory to struct dog_t
+* @d: struct dog data
+* Return: void
+*/
 void free_dog(dog_t *d)
 {
 	if (d != NULL)
 	{
 		if (d->name != NULL)
 			free(d->name);
-		if (d->age != NULL)
-			free(d->age);
 		if (d->owner != NULL)
 			free(d->owner);
 		free(d);
 	}
-	else
-		return;
 }
