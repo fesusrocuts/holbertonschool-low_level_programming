7106c32c345b744018f14ad7b40bffeb6918adda
Fesus Rocuts <fesusrocuts@gmail.com>
2019-03-14 18:44:09 +0000

    t1 c2

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -11,11 +11,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
-	if (array != NULL || size > 0 || action != NULL)
+	if (array != NULL || size > 0 || *action != NULL)
 	{
 		while (i < size)
 		{
-			action(array[i]);
+			(*action)(array[i]);
 			i++;
 		}
 	}
