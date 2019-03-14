e4c7f65d43e68df8f6ddb1883253383d74b130ac
Fesus Rocuts <fesusrocuts@gmail.com>
2019-03-14 18:30:27 +0000

    t1 c0

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+* array_iterator - execute a function using array as params
+* @array: params
+* @size: dim
+* @action: is the function
+* Return: void
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+	unsigned int n = size;
+
+	printf("n is %d\n", n);
+	if (array != NULL || n > 0 || action != NULL)
+	{
+		while (i < n)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
+	else
+		return;
+}
--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -3,3 +3,7 @@
 void print_name(char *name, void (*f)(char *));
 #endif
 
+#ifndef _ARRAY_ITARATOR_H
+#define _ARRAY_ITERATOR_H
+void array_iterator(int *array, size_t size, void (*action)(int));
+#endif
