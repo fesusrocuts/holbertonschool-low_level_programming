e73483c5f0f172ad18674664bca770695affc2a6
Fesus Rocuts <fesusrocuts@gmail.com>
2019-03-14 17:28:02 +0000

    t0 c0

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+* print_name - print name through dinamic function
+* @name: string value
+* @f: funtion that work
+* Return: void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0E-function_pointers/function_pointers.h
@@ -0,0 +1,5 @@
+#ifndef _PRINT_NAME_H
+#define _PRINT_NAME_H
+void print_name(char *name, void (*f)(char *));
+#endif
+
