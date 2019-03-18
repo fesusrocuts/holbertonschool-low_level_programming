683609de1e818af5e3f8df759776afa34eb26477
fesus rocuts <fesusrocuts@gmail.com>
2019-03-17 23:53:19 -0500

    t1 c0

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 /**
  * print_numbers - prints numbers, followed by a new line
@@ -9,5 +10,19 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	return (0);
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, unsigned int));
+
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(args);
 }
