ffdb8a92cd2cc6f3fe8d54cd79d5688137fee727
fesus rocuts <fesusrocuts@gmail.com>
2019-03-17 23:59:42 -0500

    t4 c

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 /**
  * print_strings - prints strings, followed by a new line
@@ -9,5 +10,21 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	return (0);
+	unsigned int i;
+	char *str;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
 }
--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -9,5 +9,46 @@
  */
 void print_all(const char * const format, ...)
 {
-	return (0);
+	va_list args;
+	int i = 0;
+	char *str = NULL;
+
+	va_start(args, format);
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", (char) va_arg(args, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(args, int));
+			break;
+		case 'f':
+			printf("%f", (float) va_arg(args, double));
+			break;
+		case 's':
+			str = va_arg(args, char *);
+			if (str == NULL)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", str);
+			break;
+		default:
+			i++;
+			continue;
+		}
+		if (format[i + 1] == '\0')
+		{
+			printf("\n");
+			va_end(args);
+			return;
+		}
+		printf(", ");
+		i++;
+	}
+	printf("\n");
+	va_end(args);
 }
