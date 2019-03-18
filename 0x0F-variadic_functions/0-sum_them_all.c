cb90af11e44f8ee1e525a40001fd2a2c40037bc8
fesus rocuts <fesusrocuts@gmail.com>
2019-03-17 23:47:34 -0500

    t0 c5

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -13,11 +13,11 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int s;
 	va_list args;
 
-	if(n == 0)
+	if (n == 0)
 		return (0);
 
 	va_start(args, n);
-	for(i = 0, s = 0; i < n; i++)
+	for (i = 0, s = 0; i < n; i++)
 		s += va_arg(args, int);
 	va_end(args);
 
