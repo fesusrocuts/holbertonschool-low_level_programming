14ef7375508639635e43b7030ef89f480ef58147
fesus rocuts <fesusrocuts@gmail.com>
2019-03-17 22:54:16 -0500

    t0 c0

--- 0x0F-variadic_functions/0-sum_them_all.c
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 /**
  * sum_them_all - sum of all its parameters
@@ -8,5 +9,17 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	return (0);
+	unsigned int i;
+	unsigned int s;
+	va_list o;
+
+	va_start(o, n);
+
+	if (n == 0)
+		return (0);
+	for (i = 0, sum = 0; i < n; i++)
+		sum = sum + va_arg(o, int);
+	va_end(o);
+
+	return (s);
 }
