ee421709ce801425c0c45d1b0726c1f52623529f
fesus rocuts <fesusrocuts@gmail.com>
2019-03-22 00:13:18 -0500

    t0 c1

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -9,21 +9,15 @@
  */
 size_t print_list(const list_t *h)
 {
+	const list_t *ct;
 	size_t c;
 
 	c = 0;
-
-	while (h != NULL)
+	ct = h;
+	while (ct != NULL)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
-		h = h->next;
+		printf("[%d] %s\n", ct->len, ct->str);
+		ct = ct->next;
 		c++;
 	}
 	return (c);
