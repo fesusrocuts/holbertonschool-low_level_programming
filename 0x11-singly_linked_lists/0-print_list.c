25baa541cea80430a321a2f0fb07c0430bb3d99c
fesus rocuts <fesusrocuts@gmail.com>
2019-03-21 23:57:28 -0500

    fix 0-print_list.c

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -1,7 +1,7 @@
-#include "lists.h"
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
-
+#include "lists.h"
 /**
  * print_list - prints all the elements of a list_t list
  * @h: head of the linked list
@@ -9,16 +9,20 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count;
+	size_t c;
 
 	c = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
+		}
 		else
+		{
 			printf("[%d] %s\n", h->len, h->str);
+		}
 		h = h->next;
 		c++;
 	}
