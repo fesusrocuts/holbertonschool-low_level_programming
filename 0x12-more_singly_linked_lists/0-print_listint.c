a26527af2a906ee783197c817439875713c64023
fesus rocuts <fesusrocuts@gmail.com>
2019-03-25 23:44:49 -0500

    t0 c0

---
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: head of the linked list
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t c;
+
+	c = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		c++;
+	}
+	return (c);
+}
