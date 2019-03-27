b35b9d7f0a1bc898b4fa4a1148f5d9420e9f6027
fesus rocuts <fesusrocuts@gmail.com>
2019-03-26 23:12:01 -0500

    t12 c0

---
+++ 0x12-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * free_listint_safe - that prints a listint_t linked list
+ * @h: double pointer to head of linked list
+ * Description: This function should work for circular lists
+ * Return: size of the list or exit(98)
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *c, *s;
+	size_t i;
+
+	c = *h;
+  i = 0;
+	while (c != NULL)
+	{
+		i++;
+		s = c;
+		c = c->next;
+		free(s);
+
+		if (s < c)
+			break;
+	}
+	*h = NULL;
+	return (i);
+}
