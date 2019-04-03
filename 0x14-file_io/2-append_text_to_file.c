ceb65bc7ed12b920c314f7eb28cb49da0bc18c12
fesus rocuts <fesusrocuts@gmail.com>
2019-04-02 22:32:22 -0500

    t2 c0

---
+++ 0x14-file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+* append_text_to_file - appends text at the end of a file
+* @filename: path file
+* @text_content: pointer char
+* Return: 1 on success, -1 on failure
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	long int i = 0;
+	int fo;
+	long int st = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fo = open(filename, O_WRONLY | O_TRUNC, 0600);
+	if (fo < 0)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		while (text_content[i] != '\0')
+			i++;
+		st = write(fo, text_content, i);
+		if (st != i || st < 0)
+		{
+			return (-1);
+		}
+	}
+	if (close(fo) > 0)
+	{
+		return (-1);
+	}
+	return (1);
+}
