d2ad7a4db199e322c986de60d6016490eb8578ca
fesusrocuts <fesusrocuts@gmail.com>
2019-04-09 23:30:17 -0500

    Create 2-strchr.c
    t2 c0

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _strchr - a pointer to the first occurrence of the character
+ * @s: pointer
+ * @c: character
+ * Return: pointer or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] == c)
+			return (&s[i]);
+	return ('\0');
+}
