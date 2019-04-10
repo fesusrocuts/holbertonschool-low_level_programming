448db421f22e3093ed406cedd060e22037444fb2
fesusrocuts <fesusrocuts@gmail.com>
2019-04-09 23:47:03 -0500

    Update 4-strpbrk.c
    t4 c1

--- 0x06-pointers_arrays_strings/4-strpbrk.c
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -13,6 +13,6 @@ char *_strpbrk(char *s, char *accept)
 	for (a = 0; s[a] != '\0'; a++)
 		for (b = 0; accept[b] != '\0'; b++)
 			if (accept[b] == s[a])
-        return (&s[a]);
+        			return (&s[a]);
 	return (0);
 }
