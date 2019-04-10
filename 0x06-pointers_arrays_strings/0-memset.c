494d3f2694752e9b1304c567eae1830cf0e3e6cd
fesusrocuts <fesusrocuts@gmail.com>
2019-04-09 23:19:51 -0500

    Create 0-memset.c
    t0 c0

---
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _memset - that fills memory with a constant byte.
+ * @s: buffer to write on
+ * @b: constant byte
+ * @n: n bytes of the memory
+ * Description: n bytes of memory area s with constant byte b
+ * Return: a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+    s[i] = b;
+	return (s);
+}
