f6fd9c7427535fac4e81a158887073727b8406dc
fesus rocuts <fesusrocuts@gmail.com>
2019-05-05 17:56:52 -0500

    t0 c2

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -9,21 +9,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int i = 0;
-	int tsort = 1;
-
-	if (!h && h->prev != NULL)
-		tsort = 0;
+	size_t i = 0;
 
 	while (h != NULL)
 	{
 		i++;
 		printf("%d\n", h->n);
-		if (tsort == 1)
-			h = h->next;
-		else
-			h = h->prev;
-
+		h = h->next;
 	}
 	return (i);
 }
