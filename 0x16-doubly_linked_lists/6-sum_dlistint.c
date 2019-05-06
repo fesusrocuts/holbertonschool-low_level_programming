f907f370857eab63841dc8b589a59d2a0f18d315
fesus rocuts <fesusrocuts@gmail.com>
2019-05-05 23:58:56 -0500

    t6 c0

---
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * sum_dlistint - the sum of all the data (n) of a dlistint_t linked list
+ * @head: list
+ * Return: list is empty, return 0
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
