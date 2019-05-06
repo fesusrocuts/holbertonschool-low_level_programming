023103ae525aaeea21c742e1d29a3e428f4490bd
fesus rocuts <fesusrocuts@gmail.com>
2019-05-05 23:18:43 -0500

    t2 c1

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -12,12 +12,18 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *nd = NULL;
 
+	if (!head)
+		return (NULL);
+
 	nd = malloc(sizeof(dlistint_t));
 	if (!nd)
 		return (NULL);
+
 	nd->n = n;
 	nd->next = *head;
 	nd->prev = NULL;
 	*head = nd;
+	if (nd->next && nd->next->prev == NULL)
+		nd->next->prev = nd;
 	return (nd);
 }
