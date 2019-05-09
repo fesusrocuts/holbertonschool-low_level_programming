83a19b60c704a16f59bf403a74b0a7903fd43ea8
fesus rocuts <fesusrocuts@gmail.com>
2019-05-08 22:23:44 -0500

    t5 c1

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
-wget -q -O /tmp/librand.so https://github.com/fesusrocuts/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/librand.so
-export LD_PRELOAD=/tmp/librand.so
+#!/bin/bash
+gcc -fPIC -c *c 
+gcc -shared -o liball.so *o
