3c81afa6c1e3934e206fda8ea8fed87789e53aaf
fesus rocuts <fesusrocuts@gmail.com>
2019-05-12 23:17:27 -0500

    t7 cx1 rewriting the algorithm

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,34 +11,37 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *prev;
+	dlistint_t *new, *c, *p;
 	unsigned int i = 0;
 
 	if (!h || idx < i || !n)
 		return (NULL);
 
-	prev = *h;
-	while (prev != NULL && i < idx)
-	{prev = prev->next;
+	c = *h;
+	while (c != NULL && i < idx)
+	{p = c;
+		c = c->next;
 		i++; }
-
 	if (i == idx)
 	{
 		new = malloc(sizeof(dlistint_t));
 		if (new == NULL)
 			return (NULL);
-
-		if (prev != NULL && i > 0)
-		{new->n = n;
-			new->prev = prev;
-			new->next = prev->next;
-			if (prev->next != NULL)
-				prev->next->prev = new;
-			prev->next = new; }
+		new->n = n;
+		new->prev = NULL;
+		new->next = NULL;
+		if (c != NULL && i > 0)
+		{new->prev = c->prev;
+			new->next = c;
+			c->prev = new;
+			if (new->prev != NULL)
+				new->prev->next = new;
+		}
+		else if (c == NULL && p != NULL && i > 0)
+		{p->next = new;
+			new->prev = p; }
 		else if (i == 0)
-		{new->n = n;
-			new->prev = NULL;
-			new->next = *h;
+		{new->next = *h;
 			if (new->next != NULL)
 				new->next->prev = new;
 			*h = new; }
