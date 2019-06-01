ee8fc6f3c1a86fe075889d606bdae8116ea160f1
fesus rocuts <fesusrocuts@gmail.com>
2019-05-31 23:56:51 -0500

    version without checker

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -10,10 +10,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	p1 = hast_table_t *t;
+	unsigned long int i;
+	hash_table_t *new_table = NULL;
 
-	if (!size || size < 0)
+	new_table = malloc(sizeof(hash_table_t));
+	if (!new_table)
 		return (NULL);
-
-	return (p1);
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t) * size);
+	if (!new_table->array)
+	{
+		free(new_table);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		(new_table->array)[i] = NULL;
+	return (new_table);
 }
