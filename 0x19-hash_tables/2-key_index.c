671d0105ea0261ca08facad015ed71d55569d776
fesus rocuts <fesusrocuts@gmail.com>
2019-05-31 23:58:50 -0500

    version without checker

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -11,5 +11,8 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return (0);
+	unsigned long int hash_key;
+
+	hash_key = hash_djb2((unsigned char *)key);
+	return (hash_key % size);
 }
