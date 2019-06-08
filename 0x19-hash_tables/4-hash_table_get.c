0f927591684ff4c07a6e7f1c01bf2da3ca9f9646
fesus rocuts <fesusrocuts@gmail.com>
2019-06-07 23:20:42 -0500

    fix some task

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -12,5 +12,19 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	return ("");
+	unsigned long int index;
+	hash_node_t *temp;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	    key == NULL || strlen(key) == 0)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+		temp = temp->next;
+	}
+	return (NULL);
 }
--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -8,4 +8,25 @@
  * @ht: hash table to print
  */
 void hash_table_print(const hash_table_t *ht)
-{}
+{
+	unsigned long int i;
+	hash_node_t *temp;
+	char flag = 0;
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", temp->key, temp->value);
+			flag = 1;
+			temp = temp->next;
+		}
+	}
+	printf("}\n");
+}
